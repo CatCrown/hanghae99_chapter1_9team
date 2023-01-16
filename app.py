1f9f1d2589746fe88b832e171e2459867ac6240f
Shanyu Li <uhaha529@gmail.com>
2023-01-17 01:22:05 +0900

    기능 삭제 및 부분 수정

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def result_post():
         msg = '졌다'
 
     doc = {'win': win_receive, 'msg': msg}
-    db.winner.insert_one(doc)
+    # db.history.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
 
 
@@ -41,52 +41,54 @@ def result_post():
 @app.route("/rsp", methods=["GET"])
 def rsp_get():
     rsp_list = list(db.rsp_user.find({}, {'_id': False}))
-
-
-@app.route("/rsp", methods=["POST"])
-def rsp():
-    # 이게 버튼 눌렀을 때 일어나야 되는 일
-
-    # 유저가 낸 것 user_give로 받아오기
-    user_receive = request.form['user_give']
-
-    # 컴퓨터가 낸 것 com_give로 받아오기
-    com_receive = request.form['com_give']
-
-    # 결과 result_give로 받아오기
-    result_receive = request.form['result_give']
-
-    # 히스토리 조회부분
-    history_list = list(db.rsptest.find({}, {'_id': False}))
-    count = len(history_list) + 1
-
-    doc = {
-        # 몇회차인지
-        'num': count,
-
-        # 유저가 낸 것
-        'user': user_receive,
-
-        # 컴퓨터가 낸 것
-        'com': com_receive,
-
-        # 결과
-        'result': result_receive
-    }
-
-    # 데이터를 rsptest db에 삽입
-    db.rsptest.insert_one(doc)
-
-    return jsonify({'msg': '처음 조회'})
-
-
-@app.route("/rsp/history", methods=["GET"])
-def history():
-    history_list = list(db.rsptest.find({}, {'_id': False}))
-    return jsonify({'history': history_list})
+    return jsonify({'rsp': rsp_list})
 
 
 
+# @app.route("/rsp", methods=["POST"])
+# def rsp():
+#     # 이게 버튼 눌렀을 때 일어나야 되는 일
+#
+#     # 유저가 낸 것 user_give로 받아오기
+#     user_receive = request.form['user_give']
+#
+#     # 컴퓨터가 낸 것 com_give로 받아오기
+#     com_receive = request.form['com_give']
+#
+#     # 결과 result_give로 받아오기
+#     result_receive = request.form['result_give']
+#
+#     # 히스토리 조회부분
+#     history_list = list(db.rsptest.find({}, {'_id': False}))
+#     count = len(history_list) + 1
+#
+#     doc = {
+#         # 몇회차인지
+#         'num': count,
+#
+#         # 유저가 낸 것
+#         'user': user_receive,
+#
+#         # 컴퓨터가 낸 것
+#         'com': com_receive,
+#
+#         # 결과
+#         'result': result_receive
+#     }
+#
+#     # 데이터를 rsptest db에 삽입
+#     db.history.insert_one(doc)
+#
+#     return jsonify({'msg': '처음 조회'})
+
+# 히스토리 조회 및 확인
+# @app.route("/rsp/history", methods=["GET"])
+# def history():
+#     history_list = list(db.rsptest.find({}, {'_id': False}))
+#     return jsonify({'history': history_list})
+
+
+# 컴퓨터 쪽
 # db(rsp_com)에서 image와 넘버 번호 리스트를 가져오고 랜덤으로 하나만 가져오기
 @app.route("/rsp/random", methods=["GET"])
 def rsp_get_rand():
@@ -95,22 +97,22 @@ def rsp_get_rand():
     return jsonify({'rsp_rand': rsp_ran_list})
 
 
-@app.route("/show", methods=["GET"])
-def result_show():
-    # 조회
-    history_list = list(db.history.find({}, {'_id': False}))
-
-    # 확인용
-    print(history_list)
-    return jsonify({'abc': history_list})
-
-
-@app.route("/delete", methods=["POST"])
-def result_delete():
-    # 삭제
-    history_list = list(db.history.find({}, {'_id': False}))
-    db.history.drop()
-    return jsonify({'abc': history_list})
+# @app.route("/show", methods=["GET"])
+# def result_show():
+#     # 조회
+#     history_list = list(db.history.find({}, {'_id': False}))
+#
+#     # 확인용
+#     print(history_list)
+#     return jsonify({'abc': history_list})
+#
+#
+# @app.route("/delete", methods=["POST"])
+# def result_delete():
+#     # 삭제
+#     history_list = list(db.history.find({}, {'_id': False}))
+#     db.history.drop()
+#     return jsonify({'abc': history_list})
 
 
 if __name__ == '__main__':
