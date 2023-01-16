9ae6eeaf061cb425cd707a507601f4ad1bc80349
vegeterianlee <max322318@gmail.com>
2023-01-16 16:44:52 +0900

    1. 주석 추가 2. 이기고,비기고, 졌을 때 다른 메세지가 뜨도록 변경

--- app.py
+++ app.py
@@ -11,47 +11,39 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
-@app.route("/rsp", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    count = len(bucket_list)+1
+# log값 받기 (win_give는 이긴 횟수, result_give는 승,무,패 구별하는 변수
+# result_give가 1은 비긴 것 2는 이긴 것 3은 진 것
+@app.route("/rsp_log", methods=["POST"])
+def result_post():
+    win_receive = request.form['win_give']
+    result_receive = request.form['result_give']
+    msg=''
+    if result_receive == 1:
+                            msg = '비겼다'
 
-    doc = {'num': count,'bucket': bucket_receive, 'done': 0}
-    db.bucket.insert_one(doc)
+
+   else if result_receive == 2:
+                              msg = '이겼다'
+
+        else if result_reveive == 3:
+            msg = '졌다'
+    doc = {'win': win_receive,'msg': msg}
+    db.winner.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give'] #문자열로 바뀌어서, 밑에 숫자로 바꿈
-    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    return jsonify({'msg': '버킷 완료!'})
-
-@app.route("/bucket/undone", methods=["POST"])
-def bucket_undone():
-    num_receive = request.form['num_give'] #문자열로 바뀌어서, 밑에 숫자로 바꿈
-    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
-    return jsonify({'msg': '실행 취소!'})
-
-
+#db(rsp_user)에서 image와 넘버 번호 리스트를 가져온다
 @app.route("/rsp", methods=["GET"])
 def rsp_get():
     rsp_list = list(db.rsp_user.find({}, {'_id': False}))
 
     return jsonify({'rsp': rsp_list})
 
+#db(rsp_com)에서 image와 넘버 번호 리스트를 가져오고 랜덤으로 하나만 가져오기
 @app.route("/rsp/random", methods=["GET"])
 def rsp_get_rand():
     rsp_list = list(db.rsp_com.find({}, {'_id': False}))
     rsp_ran_list = random.choice(rsp_list)
     return jsonify({'rsp_rand': rsp_ran_list})
-
-@app.route("/bucket/delete", methods=["POST"])
-def bucket_delete():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': "삭제 완료"})
 
 
 if __name__ == '__main__':
