711e011c7de48cce0f3e2b4ed2449cb624769990
siyaness <siyaness@naver.com>
2023-01-16 17:23:05 +0900

    app.py 들여쓰기 수정

--- app.py
+++ app.py
@@ -70,7 +70,8 @@ def delete():
     db.rsptest.drop()
 
     return jsonify({'history': history_list})
-=======
+
+
 # log값 받기 (win_give는 이긴 횟수, result_give는 승,무,패 구별하는 변수
 # result_give가 1은 비긴 것 2는 이긴 것 3은 진 것
 @app.route("/rsp_log", methods=["POST"])
@@ -78,15 +79,17 @@ def result_post():
     win_receive = request.form['win_give']
     result_receive = request.form['result_give']
     msg=''
+
     if result_receive == 1:
-                            msg = '비겼다'
+        msg = '비겼다'
+
+    elif result_receive == 2:
+        msg = '이겼다'
+
+    elif result_receive == 3:
+        msg = '졌다'
 
 
-   else if result_receive == 2:
-                              msg = '이겼다'
-
-        else if result_reveive == 3:
-            msg = '졌다'
     doc = {'win': win_receive,'msg': msg}
     db.winner.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
@@ -95,13 +98,9 @@ def result_post():
 @app.route("/rsp", methods=["GET"])
 def rsp_get():
     rsp_list = list(db.rsp_user.find({}, {'_id': False}))
->>>>>>> 9ae6eeaf061cb425cd707a507601f4ad1bc80349
 
 
-<<<<<<< HEAD
 
-
-=======
 #db(rsp_com)에서 image와 넘버 번호 리스트를 가져오고 랜덤으로 하나만 가져오기
 @app.route("/rsp/random", methods=["GET"])
 def rsp_get_rand():
