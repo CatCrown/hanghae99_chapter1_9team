553577b0c3e40c848df6eaa9921b756c315d5dab
Shanyu Li <uhaha529@gmail.com>
2023-01-17 00:51:54 +0900

    오류 수정

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, jsonify, random
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
+import random
 
 from pymongo import MongoClient
 
