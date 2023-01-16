ac8534752eee99655a7706ec5d23918d3a0e955f
wuzudog <121739967+wuzudog@users.noreply.github.com>
2023-01-16 16:29:31 +0900

    mongodb와 연결

---
+++ db.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+client = MongoClient(
+        "mongodb+srv://HANGHAE99_CHAPTER1_9TEAM:<password>@cluster0.fc6zoao.mongodb.net/?retryWrites=true&w=majority")
+db = client.recommendplace
